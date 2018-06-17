5dc091e0a81b4000cc458409d9c2252b19826682
allenlee820202 <allenlee820202@gmail.com>
2018-06-17 11:14:37 +0800

    Refractoring Create a folder for each chapter's sample code.

---
+++ Chapter2/src/main/java/tacos/DesignTacoController.java
@@ -0,0 +1,62 @@
+package tacos;
+
+import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.Ingredient.Type;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+	private static final Logger log = org.slf4j.LoggerFactory.getLogger(DesignTacoController.class);
+
+	@GetMapping
+	public String showDesignForm(Model model) {
+		List<Ingredient> ingredients = Arrays.asList(
+			new Ingredient("FLTO", "Flour Tortilla", Type.WRAP), 
+			new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+			new Ingredient("GRBF", "Ground Beef", Type.PROTEIN), 
+			new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+			new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES), 
+			new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+			new Ingredient("CHED", "Cheddar", Type.CHEESE),
+			new Ingredient("JACK", "Monterrey Jack", Type.CHEESE), 
+			new Ingredient("SLSA", "Salsa", Type.SAUCE),
+			new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+		);
+		
+		Type[] types = Ingredient.Type.values();
+		for (Type type : types) {
+			model.addAttribute(type.toString().toLowerCase(),
+                    ingredients.stream().
+                            filter(ingredient -> ingredient.getType().equals(type)).collect(Collectors.toList()));
+		}
+		
+		model.addAttribute("design", new Taco());
+		
+		return "design";
+		
+	}
+	
+	@PostMapping
+	public String processDesign(@Valid Taco design, Errors errors) {
+		if (errors.hasErrors()) {
+		    return "design";
+        }
+		// Save the taco design...
+		// We'll do this in chapter 3 log.info("Processing design: " + design);
+        log.info("Processing design: " + design);
+		return "redirect:/orders/current";
+	}
+
+}
