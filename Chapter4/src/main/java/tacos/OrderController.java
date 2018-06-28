be24abf1d9308acc4a96d538a05986842f35915f
allenlee820202 <allenlee820202@gmail.com>
2018-06-28 18:13:04 +0800

    Chapter 4 Security

---
+++ Chapter4/src/main/java/tacos/OrderController.java
@@ -0,0 +1,46 @@
+package tacos;
+
+import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import tacos.data.Order;
+import tacos.data.OrderRepository;
+
+@Slf4j 
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("order")
+public class OrderController {
+
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+
+        if (errors.hasErrors()) {
+		    return "orderForm";
+        }
+
+        orderRepo.save(order);
+        sessionStatus.setComplete();
+        log.info("Order submitted: " + order);
+
+        return "redirect:/";
+    }
+}
