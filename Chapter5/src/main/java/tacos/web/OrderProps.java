c5e213bab5af67d8647ffe9e02d25d02b1ccd1cb
allenlee820202 <allenlee820202@gmail.com>
2018-07-13 16:43:24 +0800

    Chapter 5 - Configuration

--- Chapter5/src/main/java/tacos/web/OrderProps.java
+++ Chapter5/src/main/java/tacos/web/OrderProps.java
@@ -10,17 +10,10 @@ import org.springframework.validation.annotation.Validated;
 @Component
 @ConfigurationProperties(prefix="taco.orders")
 @Validated
+@Data
 public class OrderProps {
 
-    // @Min(value=5, message="must be between 5 and 25")
-    // @Max(value=25, message="must be between 5 and 25")
+    @Min(value=5, message="must be between 5 and 25")
+    @Max(value=25, message="must be between 5 and 25")
     private int pageSize = 20;
-
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public int getPageSize() {
-        return pageSize;
-    }
 }
